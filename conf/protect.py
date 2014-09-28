e35705899f37245a15e660141ff8ee57a2e5d5fb
Miroslav Shubernetskiy <miroslav@miki725.com>
2014-09-28 14:55:19 -0400

    added protect script to help encrypt/decrypt sensitive files in codebase this is necessary since the project is open-source

--- conf/protect.py
+++ conf/protect.py
@@ -1,2 +1,75 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import print_function, unicode_literals
+import argparse
+import getpass
+import os
+import subprocess
+
+
+ENV_VAR = 'PROTECTION_PASSWORD'
+PROTECTED = '.protected'
+FILES = (
+    'nginx/ssl/tno.io.private.key{}',
+    '../tno/conf/newrelic.ini{}',
+    '../tno/tno/settings/prod.py{}',
+)
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '-d', '--decrypt',
+    action='store_true',
+    default=False,
+)
+
+
+class IncorrectPassword(Exception):
+    pass
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    password = getpass.getpass()
+    os.environ[ENV_VAR] = password
+
+    encrypting = 'Decrypting' if args.decrypt else 'Encrypting'
+
+    try:
+        for f in FILES:
+            if args.decrypt:
+                source = f.format(PROTECTED)
+                destination = f.format('')
+            else:
+                source = f.format('')
+                destination = f.format(PROTECTED)
+
+            if not os.path.exists(source):
+                raise Exception('{} not found'.format(source))
+
+            print('{} file {}'.format(encrypting, source))
+
+            cmd = filter(None, [
+                'openssl',
+                'aes-256-cbc',
+                '-base64',
+                '-salt',
+                '-d' if args.decrypt else None,
+                '-pass',
+                'env:{}'.format(ENV_VAR),
+                '-in',
+                source,
+                '-out',
+                destination,
+            ])
+
+            try:
+                subprocess.check_output(cmd)
+            except subprocess.CalledProcessError:
+                raise IncorrectPassword('Incorrect password')
+
+    except Exception as e:
+        parser.error(e.message)
+
+    finally:
+        del os.environ[ENV_VAR]
